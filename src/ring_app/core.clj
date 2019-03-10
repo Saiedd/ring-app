fc5f4d598a6940b2288d59ed439ccb1fb94bffe4
saiedd <ahmedsaiedsaad127@gmail.com>
2019-03-10 02:40:37 +0200

    handling Middlewares

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,16 +1,22 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
-(defn handler [request-map]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str "<html><body> Your IP is: " (:remote-addr request-map) "</body></html>")
-   }
-)
+(defn handler "(:remote-addr request-map)" [request-map]
+  (response/response
+    (str "<html><body> Your IP is: " (:remote-addr  request-map) "</body></html>")))
+
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache")))
+  )
 
 (defn -main []
-  (jetty/run-jetty handler
-  {:port 3000
-   :join? false}
+  (jetty/run-jetty (-> #'handler wrap-nocache wrap-reload)
+                   {:port 3000
+                    :join? false}
+                   )
   )
-)
